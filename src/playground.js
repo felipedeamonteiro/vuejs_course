2d8f8b6fe337e9d5d926bba2821ead32539bc54e
Felipe Monteiro <felipedeamonteiro@gmail.com>
2022-06-17 12:32:27 -0300

    tailwind config add, add playground, add mainNav

--- src/playground.js
+++ src/playground.js
@@ -1,4 +1,27 @@
-const fruits = ["Apple", "Banana", "Orange"];
-const vegetables = ["Cucumber", "Radish"];
+// Array Spreading
 
-console.log([...fruits, ...vegetables]);
+// const fruits = ["Apple", "Banana", "Orange"];
+// const vegetables = ["Cucumber", "Radish"];
+
+// console.log([...fruits, ...vegetables]);
+
+//===================================================
+// JS Objects, props and methods
+
+const developer = {
+  salary: 1000000,
+  experience: 4.5,
+  teckStack: ["Vue", "HTML", "CSS"],
+  lookingForWork: true,
+  doubleSalary() {
+    this.salary = this.salary * 2;
+    this.lookingForWork = false;
+  },
+};
+console.log(developer.salary);
+console.log(developer.lookingForWork);
+
+developer.doubleSalary();
+
+console.log(developer.salary);
+console.log(developer.lookingForWork);
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,18 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: ["./public/**/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  purge: {
+    content: ["./public/**/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  },
   theme: {
     extend: {
       fontFamily: {
         sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        "brand-gray-1": "#dadce0",
+        "brand-blue-1": "#1967d2",
+        "brand-green-1": "#137333",
       },
     },
   },
