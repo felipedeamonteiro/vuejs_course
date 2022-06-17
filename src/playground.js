601471c699da3605cdbf242fb8293b2adb8aa2c3
Felipe Monteiro <felipedeamonteiro@gmail.com>
2022-06-17 18:45:06 -0300

    add test to project

--- src/playground.js
+++ src/playground.js
@@ -8,20 +8,29 @@
 //===================================================
 // JS Objects, props and methods
 
-const developer = {
-  salary: 1000000,
-  experience: 4.5,
-  teckStack: ["Vue", "HTML", "CSS"],
-  lookingForWork: true,
-  doubleSalary() {
-    this.salary = this.salary * 2;
-    this.lookingForWork = false;
-  },
+//const developer = {
+//   salary: 1000000,
+//   experience: 4.5,
+//   teckStack: ["Vue", "HTML", "CSS"],
+//   lookingForWork: true,
+//   doubleSalary() {
+//     this.salary = this.salary * 2;
+//     this.lookingForWork = false;
+//   },
+// };
+// console.log(developer.salary);
+// console.log(developer.lookingForWork);
+
+// developer.doubleSalary();
+
+// console.log(developer.salary);
+// console.log(developer.lookingForWork);
+
+// Testing code ==========================]
+export const evenOrOdd = (number) => {
+  if (number % 2 === 0) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
 };
-console.log(developer.salary);
-console.log(developer.lookingForWork);
-
-developer.doubleSalary();
-
-console.log(developer.salary);
-console.log(developer.lookingForWork);
---
+++ tests/unit/playground.test.js
@@ -0,0 +1,26 @@
+import { evenOrOdd } from "@/playground.js";
+
+describe("basic math", () => {
+  it("adds two numbers", () => {
+    expect(1 + 1).toBe(2);
+  });
+
+  it("subtracts two numbers", () => {
+    expect(5 - 3).toBe(2);
+    expect(10 - 5).toBe(5);
+  });
+});
+
+describe("evenOrOdd", () => {
+  describe("when number is even", () => {
+    it("returns even for even numbers", () => {
+      expect(evenOrOdd(4)).toBe("Even");
+    });
+  });
+
+  describe("when number is odd", () => {
+    it("returns odd for odd numbers", () => {
+      expect(evenOrOdd(3)).toBe("Odd");
+    });
+  });
+});
