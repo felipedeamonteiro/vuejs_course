72adbafdb03272c8166ad093c6fbfee9e625ce05
Felipe Monteiro <felipedeamonteiro@gmail.com>
2022-06-17 19:59:27 -0300

    add test files to project

--- tests/unit/playground.test.js
+++ tests/unit/playground.test.js
@@ -1,4 +1,4 @@
-import { evenOrOdd } from "@/playground.js";
+import { evenOrOdd, multiply } from "@/playground.js";
 
 describe("basic math", () => {
   it("adds two numbers", () => {
@@ -9,18 +9,24 @@ describe("basic math", () => {
     expect(5 - 3).toBe(2);
     expect(10 - 5).toBe(5);
   });
-});
 
-describe("evenOrOdd", () => {
-  describe("when number is even", () => {
-    it("returns even for even numbers", () => {
-      expect(evenOrOdd(4)).toBe("Even");
+  describe("evenOrOdd", () => {
+    describe("when number is even", () => {
+      it("returns even for even numbers", () => {
+        expect(evenOrOdd(4)).toBe("Even");
+      });
+    });
+
+    describe("when number is odd", () => {
+      it("returns odd for odd numbers", () => {
+        expect(evenOrOdd(3)).toBe("Odd");
+      });
     });
   });
 
-  describe("when number is odd", () => {
-    it("returns odd for odd numbers", () => {
-      expect(evenOrOdd(3)).toBe("Odd");
+  describe("multiply", () => {
+    it("multiplies two numbers", () => {
+      expect(multiply(2, 3)).toBe(6);
     });
   });
 });
